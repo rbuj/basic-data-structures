50655886939b5821431021da73573e26fce3fa0d
rbuj <robert.buj@gmail.com>
2022-04-19 12:47:36 +0200

    stack: run unit test in main

--- stack/tests/check_stack.c
+++ stack/tests/check_stack.c
@@ -2,11 +2,22 @@
 #include <check.h>
 #include "../src/stack.h"
 
-START_TEST(test_money_pop)
-{
-  stack *s = NULL;
+stack *s = NULL;
 
+void
+setup (void)
+{
   create (&s, 10);
+}
+
+void
+teardown (void)
+{
+  destroy (&s);
+}
+
+START_TEST(test_stack_create)
+{
   ck_assert (is_empty (s));
   push (s, 10);
   ck_assert (!is_empty (s));
@@ -16,11 +27,34 @@ START_TEST(test_money_pop)
   ck_assert_int_eq (pop (s), 20);
   ck_assert_int_eq (pop (s), 10);
   ck_assert (is_empty (s));
-  destroy (&s);
+}
+
+Suite *
+stack_suite (void)
+{
+  Suite *s;
+  TCase *tc_core;
+
+  s = suite_create ("Stack");
+  tc_core = tcase_create ("Core");
+  tcase_add_checked_fixture (tc_core, setup, teardown);
+  tcase_add_test (tc_core, test_stack_create);
+  suite_add_tcase (s, tc_core);
+  return s;
 }
 
 int
 main (void)
 {
-  return EXIT_SUCCESS;
+  int number_failed;
+  Suite *s;
+  SRunner *sr;
+
+  s = stack_suite ();
+  sr = srunner_create (s);
+
+  srunner_run_all (sr, CK_NORMAL);
+  number_failed = srunner_ntests_failed (sr);
+  srunner_free (sr);
+  return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
